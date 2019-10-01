825ebfa59166a7bb8a1f5030aa336d37f523226d
progmiguel <55480620+progmiguel@users.noreply.github.com>
2019-10-01 13:18:19 +0200

    Ejercicio 2 de cadenas 1 resuelto

--- tema1/ejercicios/Cadenas1/Ej2.php
+++ tema1/ejercicios/Cadenas1/Ej2.php
@@ -14,18 +14,37 @@
     la dirección es válida escribe por un lado el nombre de usuario y por otro el dominio de dicha
     dirección.
     */
-    echo "<form action='Ej2.php' method='post' style='margin-top:25px;margin-right:auto;margin-left:auto;text-align:center;'>";
-    echo "<input name='correo' type='text'></input>";
-    echo "<input type ='submit' value='Validar' style='margin-left:25px;'></input>";
-    echo "</form>";
+    echo "<form action='Ej2.php' method='post' style='margin-top:25px;margin-right:auto;margin-left:auto;text-align:center;'>".PHP_EOL;
+    echo "<input name='correo' type='text'></input>".PHP_EOL;
+    echo "<input type ='submit' value='Validar' style='margin-left:25px;'></input>".PHP_EOL;
+    echo "</form>".PHP_EOL;
 
     if(isset($_POST["correo"])) {
-        $texto = $_POST["correo"];
+        $mail = $_POST["correo"];
         
-        echo $texto;
+        echo "\"$mail\"".PHP_EOL;
+        echo "<br>".PHP_EOL;
         
+        if(validarEmail($mail)){
+            echo "El correo introducido es válido.".PHP_EOL;
+        }else{
+            echo "El correo introducido NO es válido.".PHP_EOL;
+        }
     }
 
+    function validarEmail($email){
+        $position=strrpos($email, '@');
+        if($position!=-1){
+            $position=strrpos($email,'.');
+            if($position!=-1){
+                $dominio=substr($email,$position+1);
+                if($dominio=="com" || $dominio=="es" || $dominio=="net"){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
     ?>
     </body>
 </html>
