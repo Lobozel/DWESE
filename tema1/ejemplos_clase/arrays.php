7aec7fe52c4c9fee374b60f56b5fa6f2cf660d66
progmiguel <55480620+progmiguel@users.noreply.github.com>
2019-10-08 11:54:00 +0200

    mas ejemplos de arrays

--- tema1/ejemplos_clase/arrays.php
+++ tema1/ejemplos_clase/arrays.php
@@ -151,6 +151,80 @@
     echo "<br>Despues de sort<br>";
     sort($aux);
     print_r($aux);
+    echo "<br><br>".PHP_EOL;
+
+    //---------------------------------
+    //Como cortar arrays array_slice
+    $array=["uno","dos","tres","cuatro","cinco","seis"];
+    $res=array_slice($array,3); //solo un parametro
+    echo "<br>".PHP_EOL;
+    print_r($res); //mostrara cuatro, cinco, seis
+    echo "<br>".PHP_EOL;
+    $res=array_slice($array,2,3); //dos parametros
+    print_r($res); //mostrara tr, cua, cin
+    echo "<br>".PHP_EOL;
+    $res=array_slice($array,-5,2);
+    print_r($res);
+    echo "<br>".PHP_EOL;
+    //array chunk dividir arrays en varios
+    $res=array_chunk($array,4);
+    print_r($res);
+    echo "<br>".PHP_EOL;
+    $res=array_chunk($array,4,true); //preserva las keys originales
+    print_r($res);
+    echo "<br>".PHP_EOL;
+
+    //explode e implode----------------------------
+    //convierten string a array o array a string
+    $cad="El cielo esta enladrillado";
+    echo "<br>$cad<br>";
+    $array=explode(" ",$cad);
+    print_r($array);
+    echo "<br>".PHP_EOL;
+    $cad="felipe::perez::felipe@correo.es::http://felipe.es";
+    $array=explode("::",$cad);
+    print_r($array);
+    echo "<br>".PHP_EOL;    
+    list($nom,$apellidos,$mail,$url)=explode("::",$cad);
+    echo "nom=$nom, ape=$apellidos, mail=$mail, url=$url<br>";
+    //implode array a String
+    $array=["portero","delanteros","centrales","defensas","suplentes"];
+    $string=implode("::",$array);
+    echo "<br>$string<br>";
+    //añadir o quitar elementos de un array
+    //Al final del mismo------------------
+    $array=["portero","delanteros","suplentes"];
+    array_push($array,"Entrenadores"); //añade entrenadores al final
+    print_r($array);
+    echo "<br>".PHP_EOL;
+    array_pop($array);
+    print_r($array);
+    echo "<br>".PHP_EOL;
+    //al principio
+    array_unshift($array, "Entrenadores")    ;
+    print_r($array);
+    echo "<br>";
+    array_shift($array);
+    print_r($array);
+    echo "<br>".PHP_EOL;
+    //Juntar dos arrays array_merge------------------
+    $array1=array(1,"dos",3,4,"cinco",9);
+    $array2=array(1,"hola",3,"adios",5,6);
+    $union=array_merge($array1, $array2);
+    print_r($union);
+    echo "<br>".PHP_EOL;
+    //si unimos array asociativos
+    $mat1=[
+        "uno"=>"valor 1 array 1",
+        "dos"=>"valor 2 array 1"
+    ];
+    $mat2=[
+        "key1"=>"valor 1 array 2",
+        "key2"=>"valor 2 array 2"
+    ];
+    $union=array_merge($mat1, $mat2);
+    print_r($union);
+    echo "<br>".PHP_EOL;
 
     echo "<br><br>".PHP_EOL;
     ?>
