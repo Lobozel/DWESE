8302809573a5d5a1e3b644ff40cf80468db2eee4
progmiguel <55480620+progmiguel@users.noreply.github.com>
2019-10-16 12:52:35 +0200

    ejemplos php

--- ejemplos_clase/formularios/subirfile.php
+++ ejemplos_clase/formularios/subirfile.php
@@ -6,28 +6,103 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     </head>
+
+    <?php
+
+        function btnVolver(){
+            ?>
+            <p class='text-center mt-5'>
+            <a href='<?php echo $_SERVER['PHP_SELF'];?>' class='btn btn-primary'>Volver</a>
+            </p>
+            <?php
+        }
+
+        function mierror($msj){
+            echo "<div class='cointainer text-center mt-5'>";
+            echo "<p class='text-danger text-weight-bold'>$msj</p>";
+            btnVolver();
+            echo "</div>";
+            die();
+        }
+    ?>
+
     <body style='background-color:bisque'>
+<?php
+    //funcion ver ficha
+    function verFicha($n,$e,$f){
+
+        ?>
+
+        <div class='container mt-5 border border-dark text-center'>
+            <table border='0' cellpadding='3' cellspacing='3'>
+                <tr align='center'>
+                    <td colspan='2' align='center'><b>FICHA</b></td>
+                </tr>
+                <tr>
+                    <td colspan='2'><img src='<?php echo $f ?>' width='200px' height='200px' class='rounded-circle'></td>
+                </tr>
+                <tr>
+                    <td><b>Nombre:</b></td>
+                </tr>
+                <tr>
+                    <td><?php echo $n?></td>
+                </tr>
+                <tr>
+                    <td><b>E-mail:</b></td>
+                </tr>
+                <tr>
+                    <td><?php echo $e?></td>
+                </tr>
+            </table>
+        </div>
+
+        <?php
+
+    }
+?>
+
     <?php
 if(isset($_POST['btnEnviar'])){
     //hemos pulsado enviar, procesaremos los datos
 
+    if($_FILES['foto']['error']==2){
+        mierror("El archivo excede el tamaño permitido!!!");
+    }
+
     // var_dump($_FILES['foto']);
     // Paso 1 compruebo que efectivamente se ha subido el archivo (en principio en temp)
     if(is_uploaded_file($_FILES['foto']['tmp_name'])){
+
+        $array=[
+            'image/jpeg',
+            'image/png',
+            'image/tiff',
+            'image/bmp',
+            'image/gif',
+            'image/x-icon',
+            'image/svg+xml'
+        ];
+        if(!in_array($_FILES['foto']['type'],$array)){
+            mierror("No es un archivo de imágen!!!!");
+        }
+
         //El archivo se subio correctamente
+        //Cogemos nombre y correo
+        $n=$_POST['nombre'];
+        $e=$_POST['email'];
         $nombreActual=$_FILES['foto']['name'];
         $id=time(); //marca de tiempo es un entero secuencial y único
-        $nombre=$id.$nombreActual; //le meto el id delante como nombre de archivo
+        $nombre='./perfiles/'.$id.$nombreActual; //le meto el id delante como nombre de archivo
         move_uploaded_file($_FILES['foto']['tmp_name'], $nombre);
-    }else{
-        echo "<br>Error, NO se subió el Archivo!!!!!!";
-    }
 
-    ?>
-    <p class='text-center mt-5'>
-    <a href='<?php echo $_SERVER['PHP_SELF'];?>' class='btn btn-primary'>Volver</a>
-    </p>
-    <?php
+        verFicha($n,$e,$nombre);
+
+    }else{
+        mierror("No se subió ningún archivo");
+    }
+    btnVolver();
+
+    
     
 }else{
     //Pinto el formulario
@@ -36,13 +111,14 @@ if(isset($_POST['btnEnviar'])){
         <h3 class='text-center bg-primary'>Subir Archivos</h3>
         <!--ENCTYPE necesario para subida de ficheros-->
         <form name='sf' action='<?php echo $_SERVER['PHP_SELF']; ?>' ENCTYPE='multipart/form-data' method='POST'>
-            <input type='text' name='nombre' placeholder='Tú Nombre' /><br>
+            <input type='text' name='nombre' placeholder='Tú Nombre' required /><br>
+            <input type='email' name='email' placeholder='Tú Nombre' required /><br>
             <b>Tu Foto</b>&nbsp;<input type='file' name='foto'/>
             <p class='text-center mt-5'>
                 <input type='submit' value='Enviar' name='btnEnviar' class='btn btn-success'/>
             </p>
             <!--Si quiero imponer un tamaño máximo al fichero-->
-            <input type='hidden' name='MAX_FILE_SIZE' value='20000'/>
+            <input type='hidden' name='MAX_FILE_SIZE' value='2000'/>
         </form>
     </div>
 <?php
