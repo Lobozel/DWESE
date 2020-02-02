5907a2976b40b057ff6033a870da60d139db9c0e
Lobozel <33419952+Lobozel@users.noreply.github.com>
2020-02-02 23:13:21 +0100

    Coches Index finalizada

--- ejemplos_clase/laravel/concesionario/app/Coche.php
+++ ejemplos_clase/laravel/concesionario/app/Coche.php
@@ -10,6 +10,7 @@ class Coche extends Model
     protected $fillable=['matricula','modelo','color','tipo','klms','pvp','foto','marca_id'];
     //un coche tendrá una única marca en la relacion 1:n marcas coches
     public function marca(){
-        return $this->belongsTo(Marca::class);
+        return $this->belongsTo(Marca::class)
+        ->withDefault(['nombre'=>'Sin Marca']);
     }
 }
