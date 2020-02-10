8c92f6946fc772a42029e842932a5f73f985fbe8
Lobozel <33419952+Lobozel@users.noreply.github.com>
2020-02-11 00:05:58 +0100

    academia

---
+++ ejemplos_clase/laravel/academia/app/Alumno.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Alumno extends Model
+{
+    protected $fillable =["nombre", "apellidos", "mail", "logo"];
+
+    public function modulos(){
+       return  $this->belongsToMany("App\Modulo")->as("notas")->withPivot("nota")->withTimestamps();
+    }
+}
---
+++ ejemplos_clase/laravel/academia/app/Modulo.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Modulo extends Model
+{
+    protected $fillable=["nombre", "horas"];
+
+    //Métodos para la relación con Alumnos
+
+    public function alumnos(){
+        return $this->belongsToMany("App\Alumno")->withPivot("nota");
+    }
+}
